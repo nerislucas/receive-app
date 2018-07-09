d1d9ceed0e1d47cf4409df4e5652a9dc4223b1e6
Lucas Neris <lucas.neris@globalpoints.com.br>
2018-07-09 10:05:38 -0300

    add model and interpolation in our recipe view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Receipe } from './models/receipe';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  recipe: Receipe;
+
+  constructor() {
+    this.recipe = new Receipe('Melon', 'This is my favourite melon bread recipe!', null, null, null);
+  }
 }
---
+++ src/app/models/receipe.ts
@@ -0,0 +1,29 @@
+export interface Ingredient {
+    ingredient: string;
+    measure: string;
+}
+
+export interface Instruction {
+    instruction: string;
+    photo: string;
+}
+
+export class Receipe {
+    constructor(title: string
+        , description: string
+        , ingredients: Ingredient[]
+        , instructions: Instruction[]
+        , coverPhoto: string) {
+        this.title = title;
+        this.description = description;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+        this.coverPhoto = coverPhoto;
+    }
+
+    public title: string;
+    public description: string;
+    public ingredients: Ingredient[];
+    public instructions: Instruction[];
+    public coverPhoto: string;
+}
