4005e58adef870ec23a57127d6e819eae952a808
Lucas Neris <lucas.neris@globalpoints.com.br>
2018-07-16 18:54:43 -0300

    using reactForms

--- src/app/components/new-edit-recipe-component/new-edit-recipe.component.ts
+++ src/app/components/new-edit-recipe-component/new-edit-recipe.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 import { Recipe, Ingredient, Instruction } from '../../models/recipe';
 import { RecipeService } from '../../services/recipe.service';
@@ -11,45 +12,72 @@ import { RecipeService } from '../../services/recipe.service';
 })
 export class NewEditRecipeComponent implements OnInit {
   recipe: Recipe;
+  recipeForm: FormGroup;
 
   constructor(private recipeService: RecipeService
-    , private router: Router) { }
+    , private router: Router
+    , private formBuilder: FormBuilder) { }
 
-  newOrEdit() {
-    this.recipe = Recipe.getEmptyRecipe();
-    this.recipe.ingredients = new Array<Ingredient>();
-    this.recipe.instructions = new Array<Instruction>();
+  loadFormGroup() {
+    if (!this.recipe || !this.recipe.ingredients) {
+      this.recipe = Recipe.getEmptyRecipe();
+      this.recipe.ingredients = new Array<Ingredient>();
+    }
+
+    const group = {
+      title: [this.recipe.title, Validators.required],
+      description: [this.recipe.description, Validators.required],
+      preparationTime: [this.recipe.preparationTime, Validators.required],
+      feedsMany: [this.recipe.feedsMany, Validators.required]
+    };
+
+    for (let index = 0; index < this.recipe.ingredients.length; index++) {
+      group['ingredient' + index] = [this.recipe.ingredients[index].ingredient, [Validators.required]];
+      group['measure' + index] = [this.recipe.ingredients[index].measure, [Validators.required]];
+    }
+
+    // for (let index = 0; index < this.recipe.instructions.length; index++) {
+    //   group['instruction' + index] = [this.recipe.instructions[index].instruction, [Validators.required]];
+    // }
+
+    this.recipeForm = this.formBuilder.group(group);
   }
 
   ngOnInit() {
-    this.newOrEdit();
+    this.loadFormGroup();
   }
 
   onAddIngredient() {
     const ingredient = {} as Ingredient;
     this.recipe.ingredients.push(ingredient);
+    this.loadFormGroup();
   }
 
   onRemoveIngredient(index) {
     this.recipe.ingredients.splice(index, 1);
+    this.loadFormGroup();
   }
 
   onAddInstruction() {
     const instruction = {} as Instruction;
     this.recipe.instructions.push(instruction);
+    this.loadFormGroup();
   }
 
   onRemoveInstruction(index) {
     this.recipe.instructions.splice(index, 1);
+    this.loadFormGroup();
   }
 
   saveRecipe() {
-    this.recipeService
-      .upsert(this.recipe)
-      .then(recipe => {
-        if (recipe) {
-          this.router.navigateByUrl(`recipes/${recipe.id}`);
-        }
-      });
+    console.log('this.recipeForm.value', this.recipeForm.value);
+    console.log('this.recipeForm.status', this.recipeForm.invalid);
+    // this.recipeService
+    //   .upsert(this.recipe)
+    //   .then(recipe => {
+    //     if (recipe) {
+    //       this.router.navigateByUrl(`recipes/${recipe.id}`);
+    //     }
+    //   });
   }
 }
